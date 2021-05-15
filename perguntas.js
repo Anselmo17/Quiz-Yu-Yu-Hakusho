a724124212ee47549f4b85c3c5d3f0601ba28c0b
Anselmo <ansinftec17@gmail.com>
2021-05-15 18:38:48 -0300

    Implementando novas perguntas ao repertorio

--- perguntas.js
+++ perguntas.js
@@ -53,4 +53,70 @@ let lista = [
     ],
     resposta: 2,
   },
+  {
+    pergunta: "Qual o nome de Kurama Yoko na terra?",
+    options: [
+      { 1: "Shiori Minamino" },
+      { 2: "Kurama" },
+      { 3: "Kurama Minamino" },
+      { 4: "Shiori Yoko" },
+      { 5: "Shorie Minamyno" },
+    ],
+    resposta: 1,
+  },
+  {
+    pergunta: "Qual o emprego de Yusuke apos sua ressureissão?",
+    options: [
+      { 1: "Lutador de MMA Sobrenatural" },
+      { 2: "Guarda sobrenatural" },
+      { 3: "Detetive do Makai" },
+      { 4: "Lutador das trevas" },
+      { 5: "Detetive Sobrenatural" },
+    ],
+    resposta: 5,
+  },
+  {
+    pergunta: "Quantos episodios tem Yu Yu Hakusho?",
+    options: [
+      { 1: "128" },
+      { 2: "682" },
+      { 3: "128" },
+      { 4: "112" },
+      { 5: "100" },
+    ],
+    resposta: 4,
+  },
+  {
+    pergunta: "Qual o golpe favorito de Yusuke?",
+    options: [
+      { 1: "Leigun" },
+      { 2: "Shotgun" },
+      { 3: "Big Bang Attack" },
+      { 4: "Reigan" },
+      { 5: "Kaioken" },
+    ],
+    resposta: 1,
+  },
+  {
+    pergunta: "Como se chama o personagem que roubou o Espelho das Trevas logo no inicio do anime?",
+    options: [
+      { 1: "Kurama" },
+      { 2: "Goki" },
+      { 3: "Yusuke" },
+      { 4: "Sensui" },
+      { 5: "Tiyu" },
+    ],
+    resposta: 1,
+  },
+  {
+    pergunta: "Qual é o nome do lutador que aumenta sua força quando fica embreagado?",
+    options: [
+      { 1: "Tiyu(Chu)" },
+      { 2: "Doutor Itigaki" },
+      { 3: "Toguro Ani" },
+      { 4: "Toguro Ototo" },
+      { 5: "Sakiyu" },
+    ],
+    resposta: 1,
+  },
 ];
